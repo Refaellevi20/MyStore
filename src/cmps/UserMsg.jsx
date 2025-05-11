6239a327606c8455418ec4c6a5bbe37d8cb83574
Refael Levi <refael14levi@gmail.com>
2025-05-11 21:41:50 +0300

    add usermsg

--- src/cmps/UserMsg.jsx
+++ src/cmps/UserMsg.jsx
@@ -1,44 +1,64 @@
-import { useState, useEffect, useRef } from 'react';
-import { eventBus, showSuccessMsg, showErrorMsg } from '../services/event-bus.service';
-import { IoCheckmarkCircleOutline } from "react-icons/io5";
-import { HiMiniXMark } from "react-icons/hi2";
+import { eventBus, showSuccessMsg  } from '../services/event-bus.service.js'
+import { useState, useEffect, useRef } from 'react'
+import { socketService, SOCKET_EVENT_NEW_MESSAGE, SOCKET_EVENT_ORDER_FOR_YOU, SOCKET_EVENT_ORDER_UPDATED } from "../services/socket.service.js"
+import { useDispatch, useSelector } from "react-redux"
 
 export function UserMsg() {
-  const [msg, setMsg] = useState(null);
-  const timeoutIdRef = useRef();
+
+  const [msg, setMsg] = useState(null)
+  const timeoutIdRef = useRef()
+  const dispatch = useDispatch()
+  console.log(msg)
+  
 
   useEffect(() => {
-    // Listen for 'show-msg' events from the eventBus
     const unsubscribe = eventBus.on('show-msg', (msg) => {
-      setMsg(msg);
-
-      // Automatically close the message after 3 seconds
+      setMsg(msg)
+      // window.scrollTo({ top: 0, behavior: 'smooth' })
+      //* big problom on the window.scrollTo
       if (timeoutIdRef.current) {
-        clearTimeout(timeoutIdRef.current);
+        timeoutIdRef.current = null
+        clearTimeout(timeoutIdRef.current)
       }
-      timeoutIdRef.current = setTimeout(() => {
-        setMsg(null)
-      },3000)
+      timeoutIdRef.current = setTimeout(closeMsg, 3000)
     })
 
-    return () => unsubscribe()
+    socketService.on(SOCKET_EVENT_ORDER_FOR_YOU, (data) => {
+      // console.log(data)
+      const notification = {'type': 'order', 'data': data}
+      dispatch({type: 'NOTIFY', notification})
+      showSuccessMsg(`New order about for you`)
+    })
+
+    socketService.on(SOCKET_EVENT_NEW_MESSAGE, (data) => {
+      showSuccessMsg(`New message from ${data}`)
+    })
+
+    socketService.on(SOCKET_EVENT_ORDER_UPDATED, (data)=>{
+      const notification = {'type': 'order', 'data': data}
+      dispatch({type: 'NOTIFY', notification})
+      showSuccessMsg(`Order status has been updated to ${data.status}`)
+    })
+
+    return () => {
+      unsubscribe()
+      socketService.off(SOCKET_EVENT_ORDER_FOR_YOU)
+      socketService.off(SOCKET_EVENT_NEW_MESSAGE)
+      socketService.off(SOCKET_EVENT_ORDER_UPDATED)
+    }
   }, [])
 
   function closeMsg() {
     setMsg(null)
-    if (timeoutIdRef.current) clearTimeout(timeoutIdRef.current)
+  }
+  function onSetNotificaitons(notificaitons) {
+    console.log(notificaitons)
   }
 
-  if (!msg) return null
-
+  if (!msg) return <span></span>
   return (
-    <section className={`user-msg ${msg.type} visible`}>
-      <div className='svg-icon-msg'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#3FCA89" viewBox="0 0 16 16"><path fillRule="evenodd" d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m3.644-10.612a.774.774 0 0 1 0 1.095l-4.129 4.129a.774.774 0 0 1-1.095 0L4.356 8.547A.774.774 0 1 1 5.45 7.453L6.968 8.97l3.581-3.582a.774.774 0 0 1 1.095 0" clipRule="evenodd"></path></svg>
-      </div>
-      <p className='txt-msg-icon'>{msg.txt}</p>
-      <button onClick={closeMsg}><HiMiniXMark style={{ fontSize: '22px', marginLeft: '-22px', marginTop: '8px' }} />
-      </button>
+    <section className={`user-msg ${msg.type}`}>
+      {msg.txt}
     </section>
   )
 }
